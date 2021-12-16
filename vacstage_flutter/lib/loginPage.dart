537d9d7a40bca6d398f25a1902645f4617fffe2b
qhahd78 <umuj0708@likelion.org>
2021-12-16 18:48:51 +0900

    [Feat]::LoginPage Pub

--- vacstage_flutter/lib/loginPage.dart
+++ vacstage_flutter/lib/loginPage.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import './common/input.dart';
+import './layouts/login.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-          height: 150,
-          child: SvgPicture.asset('assets/images/MainLogo.svg'),
-        ),
-        Container(
-          child: Column(
-            children: [
-              Input(text:"a"),
-              Input(text:"a"),
-            ],
-          )
-        )
-      ],
-    ));
+    return Container(
+      child: Login(),
+    );
   }
 }
